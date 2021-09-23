03cd304e70edaee1c9cf27f88a9c64cc8ed194b1
hlai16 <gloriajoanlai@gmail.com>
2021-09-23 19:58:02 -0400

    cats API challenges finally all completed.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,24 @@
 const request = require('request');
 
-const userInput = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${userInput}`;
+const fetchBreedDescription = function(breedName, callback) {
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  }
-  // Print the error if one occurred
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log('Breed not found.');
-  } else {
-    const result = data[0].description;
-    console.log(result);
-  }
-});
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('error:', error);
+    }
+    // Print the error if one occurred
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      callback('Breed not found', null);
+    } else {
+      const result = data[0].description;
+      callback(null, result);
+    }
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
